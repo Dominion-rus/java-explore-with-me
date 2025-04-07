4dffde65e7916e70cec08dbac14de795eb75c288
Dominion_rus <fatal1ty2013@yandex.ru>
2025-04-07 23:22:38 +0300

    init commit,create project

---
+++ stats/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
@@ -0,0 +1,20 @@
+package ru.practicum.stats.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EndpointHitDto {
+    private String app;
+    private String uri;
+    private String ip;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
