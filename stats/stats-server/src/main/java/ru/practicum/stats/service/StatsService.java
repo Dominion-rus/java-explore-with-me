ac168d81e72fbede1f5d600636a73f807c0d4557
Dominion_rus <fatal1ty2013@yandex.ru>
2025-04-10 11:04:07 +0300

    fix style

--- stats/stats-server/src/main/java/ru/practicum/stats/service/StatsService.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/service/StatsService.java
@@ -7,6 +7,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatsService {
+
     void saveHit(EndpointHitDto dto);
+
     List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
 }
